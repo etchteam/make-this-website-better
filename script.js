aadc1f14c0bef02e504cc235cca81c52852afacc
Ben <ben.lynch1931@gmail.com>
2021-01-15 14:29:34 +0000

    dropdown for address

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ console.log('Connected!')
 $(document).ready(() => {
   let viewDropdown = false
   $('.form-group__address').on('click', () => {
-      $('.form-group__address-dropdown').addClass("view")
-      $('.form-group__address').addClass("clicked")
+
+    if( viewDropdown == false ) {
+      $('.form-group__address-dropdown').addClass("view");
+      $('.form-group__address').addClass("clicked");
+      $('.form-group__address').html("X");
       viewDropdown = true
+
+    } else if ( viewDropdown == true ) {
+      $('.form-group__address-dropdown').removeClass("view");
+      $('.form-group__address').removeClass("clicked");
+      $('.form-group__address').html("Address");
+      viewDropdown = false
+    }
   })
 
-  $('.form-group__address-dropdown').on('click', () => {
-    $('.form-group__address-dropdown').removeClass("view")
-    $('.form-group__address').removeClass("clicked")
-    viewDropdown = false
-  })
 })
