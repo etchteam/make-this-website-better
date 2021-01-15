53d1f2f562d2d7566389a68f4eea4be31ce1d116
Ben <ben.lynch1931@gmail.com>
2021-01-15 14:00:42 +0000

    begin adding dropdown for address

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Connected!')
+
+
+$(document).ready(() => {
+  let viewDropdown = false
+  $('.form-group__address').on('click', () => {
+      $('.form-group__address-dropdown').addClass("view")
+      $('.form-group__address').addClass("clicked")
+      viewDropdown = true
+  })
+
+  $('.form-group__address-dropdown').on('click', () => {
+    $('.form-group__address-dropdown').removeClass("view")
+    $('.form-group__address').removeClass("clicked")
+    viewDropdown = false
+  })
+})
