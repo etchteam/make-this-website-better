8091cd4354b5dccc54d71b3f5925b567cded1d52
brownspunbear <benjaminrshill@hotmail.com>
2021-01-18 22:44:31 +0000

    tweaking

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const namePattern = new RegExp(/^(?=.{3,50}$)[a-zA-Z]+/);
 const emailPattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/);
 
 document.addEventListener('keyup', () => {
-    validateForm() ? submitButton.disabled = false : submitButton.disabled = true;
+    submitButton.disabled = !validateForm();
     whyReq.textContent = why.value.length;
 });
 
@@ -21,12 +21,7 @@ let getFormData = () => {
 let validateForm = () => {
     let success = true;
     let data = getFormData();
-    if (!namePattern.test(data.name)
-        || !emailPattern.test(data.email)
-        || data.why.length < 200
-        || data.why.length > 2000) {
+    if (!namePattern.test(data.name) || !emailPattern.test(data.email) || data.why.length < 200 || data.why.length > 2000) {
         success = false;
-    }
-    return success;
+    } return success;
 }
-
