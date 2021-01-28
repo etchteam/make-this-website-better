c61826bbf4d20467de6b638b30cb299be43ec5af
Ali <adgk.dev@gmail.com>
2021-01-28 15:09:13 +0000

    Add Javascript form validation
    Some basic form input checks using 'constraint validation API'- with immediate UI feedback

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-console.log('Connected!')
+const form = document.querySelector('form');
+
+// Basic inline form validation
+const inlineFormValidation = (form) => {
+  const requiredInputs = [...form.querySelectorAll('input')].filter(
+    (input) => input.required
+  );
+  const submitButton = form.querySelector('.form__submit');
+
+  requiredInputs.forEach((input) => {
+    formValidationOnInput(input);
+    inputValidationOnInput(input);
+    inputValidationOnBlur(input);
+  });
+
+  // Enable submit button if all required inputs are valid
+  function formValidationOnInput(input) {
+    input.addEventListener('input', (e) => {
+      submitButton.disabled = !form.checkValidity();
+    });
+  }
+
+  // Remove help on change if valid input
+  function inputValidationOnInput(input) {
+    const help = input.parentElement.parentElement.querySelector(
+      '.form-group__help'
+    );
+    input.addEventListener('input', (e) => {
+      input.checkValidity() ? (help.style.display = 'none') : null;
+    });
+  }
+
+  // Display help on blur if invalid input
+  function inputValidationOnBlur(input) {
+    const help = input.parentElement.parentElement.querySelector(
+      '.form-group__help'
+    );
+    input.addEventListener('blur', (e) => {
+      help.style.display = input.checkValidity() ? 'none' : 'block';
+    });
+  }
+};
+
+// Set footer date
+const setFooterDate = () => {
+  const dateContainer = document.getElementById('date-container');
+  dateContainer.innerText = new Date().getFullYear();
+};
+
+// Initialise
+
+function init() {
+  setFooterDate();
+  inlineFormValidation(form);
+}
+
+init();
