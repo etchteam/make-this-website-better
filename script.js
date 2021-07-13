71ce7786141db0cca3c372d771f10a04c31dd8e6
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-07-13 17:27:31 +0100

    further edits to form

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ console.log('Connected!')
 
 const submit = document.getElementById('submit')
 const form = document.querySelector('.main')
+const subheading = document.querySelector('.hero__subtitle')
 
 const submitForm = () => {
   console.log('Submitted!')
   form.innerHTML = "Thanks for your interest - we'll be in touch!";
+  subheading.innerHTML = ""
 };
 
 submit.addEventListener("click", (event) => {
