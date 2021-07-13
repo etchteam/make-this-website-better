2f5ac3f4fcacd3fc697c04687d24fda4b88f2687
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-07-13 16:38:14 +0100

    initial layout

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('Connected!')
+
+const submit = document.getElementById('submit')
+const form = document.querySelector('.main')
+
+const submitForm = () => {
+  console.log('Submitted!')
+  form.innerHTML = "Thanks for your interest - we'll be in touch!";
+};
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  submitForm()
+});
