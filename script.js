4fc8c0f253d73451855fad92ce23ca5db3efedae
Andreea <andreeailiescu.digital@gmail.com>
2021-07-15 17:00:07 +0300

    redesigned and implemented contact form page

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('Connected!')
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const background = document.querySelector(".page");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+// function lockScroll() {
+//     background.classList.toggle('lock-scroll');
+// }
