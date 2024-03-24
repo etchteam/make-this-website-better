7cf0ff53222a423e12bd52c56bd750cd820b6122
Annie Seaward <annie.seaward@gmail.com>
2024-03-24 15:11:34 +0000

    Improve form function, accessibility, style

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ window.addEventListener("scroll", (e) => {
     nav.classList.remove("add__shadow");
   }
 });
+
+//Display character counter
+let textarea = document.getElementById("why");
+let counterCharacters = document.querySelector(".counter__characters");
+
+textarea.oninput = function () {
+  counterCharacters.innerText = textarea.value.length;
+  if (textarea.value.length >= 1000) {
+    counterCharacters.classList.add("text--red");
+  } else {
+    counterCharacters.classList.remove("text--red");
+  }
+};
+
+//Enable Submit when required fields filled
+//This is not designed to prevent spam, just for styling
+enableSubmit = function () {
+  let isValid = true;
+  for (const input of inputs) {
+    if (input.value.trim() === "" || input.value === null) {
+      isValid = false;
+      break;
+    }
+  }
+  btn.disabled = !isValid;
+};
+
+let inputs = document.querySelectorAll("input[required], textArea[required]");
+let btn = document.querySelector('input[type="submit"]');
+for (const input of inputs) {
+  input.onkeyup = enableSubmit;
+}
